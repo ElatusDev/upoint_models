c9bccab21736451ec26633b50f9e6161cabee409
augusto <augustomtz95@icloud.com>
2023-05-03 21:02:31 -0600

    first commit

---
+++ src/main/java/com/upoint/model/Product.java
@@ -0,0 +1,47 @@
+package com.upoint.model;
+
+import java.util.List;
+
+public class Product {
+	private String code; 
+	private String name; 
+	private String description; 
+	private Category category;
+	private List <Property> properties;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public List<Property> getProperties() {
+		return properties;
+	}
+	public void setProperties(List<Property> properties) {
+		this.properties = properties;
+	}
+	
+	
+	
+	
+
+}
